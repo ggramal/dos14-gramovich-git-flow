4f93712aa78892e1d720b0f9a9d587ee973e87e1
Alexander Gramovich <34745780+ggramal@users.noreply.github.com>
2023-06-16 22:59:24 +0300

    Add separate urls for creating/updating/getting credit and debit acco… (#6)
    * Add separate urls for creating/updating/getting credit and debit accounts
    
    * Fix var naming
    
    * Use nested error checking

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ class DebitAccount(CommonAccount):
 
     if add > 0:
       trx = self._amount + add
-   
+
     if trx < 0:
       raise TransactionError("Debit account cant be less than 0")
 
@@ -78,76 +78,134 @@ class CreditAccount(CommonAccount):
 def read_accounts_file(account_file):
   with open(account_file, "r") as f:
     return yaml.safe_load(f)
-  
-def write_accounts_file(account_file, accounts):
+
+def write_accounts_file(account_file):
+  accounts = {
+    **all_accounts["creditaccounts"],
+    **all_accounts["debitaccounts"]
+  }
   accs = [account.to_dict() for client_id, account in accounts.items()]
   with open(account_file,"w") as f:
     yaml.dump(accs, f, allow_unicode=True)
 
 def init():
   accounts = read_accounts_file(account_file)
-  account_dict = {}
+  debit_account_dict = {}
+  credit_account_dict = {}
   for account in accounts:
       client_id = int(account["client_id"])
       if account["type"] == "credit":
-        account_dict[client_id] = CreditAccount(**account)
+        credit_account_dict[client_id] = CreditAccount(**account)
       elif account["type"] == "debit":
-        account_dict[client_id] = DebitAccount(**account)
-  return account_dict
+        debit_account_dict[client_id] = DebitAccount(**account)
+  return debit_account_dict, credit_account_dict
 
-@app.route("/api/v1/accounts", methods=["GET"])
-def read_accounts():
-  return [account.to_dict() for client_id, account in accounts.items()]
-
-@app.route("/api/v1/accounts/<int:client_id>", methods=["GET"])
-def read_account(client_id):
+@app.route("/api/v1/<string:account_type>", methods=["GET"])
+def read_accounts(account_type):
   try:
-    response = make_response(accounts[client_id].to_dict())
-  except KeyError:
-    response = make_response({"status": "error", "message": "Client not found"})
-    response.status = 404 
-  
-  return response
-
-@app.route("/api/v1/accounts/<int:client_id>/transaction", methods=["POST"])
-def transaction(client_id):
-  r = request.json
-  try:
-    account = accounts[client_id]
-    account.transaction(**r)
-    write_accounts_file(account_file, accounts)
-    response = make_response(account.to_dict())
+    accounts = all_accounts[account_type]
+    response = [account.to_dict() for client_id, account in accounts.items()]
   except KeyError as ek:
-    response = make_response({"status": "error", "message": "Client not found"})
-    response.status = 404 
-  except TransactionError as e:
-    response = make_response({"status": "error", "message": f"{e}"})
-    response.status = 400
-  except WithdrawalBlocked as e:
-    response = make_response({"status": "error", "message": f"{e}"})
-    response.status = 403
-        
+    response = make_response(
+      {
+        "status": "error",
+        "message": f"account type - {account_type}. Is a wrong account type. Must be one of {','.join(all_accounts.keys())}"
+      }
+    )
+    response.status = 404
+
   return response
 
-@app.route("/api/v1/accounts", methods=["PUT"])
-def create_account():
+@app.route("/api/v1/<string:account_type>/<int:client_id>", methods=["GET"])
+def read_account(account_type, client_id):
+  try:
+    accounts = all_accounts[account_type]
+    try:
+      response = make_response(accounts[client_id].to_dict())
+    except KeyError:
+      response = make_response({"status": "error", "message": "Client not found"})
+      response.status = 404
+  except KeyError as ek:
+    response = make_response(
+      {
+        "status": "error",
+        "message": f"account type - {account_type}. Is a wrong account type. Must be one of {','.join(all_accounts.keys())}"
+      }
+    )
+    response.status = 404
+
+  return response
+
+@app.route("/api/v1/<string:account_type>/<int:client_id>/transaction", methods=["POST"])
+def transaction(account_type, client_id):
+  try:
+    accounts = all_accounts[account_type]
+    r = request.json
+    try:
+      account = accounts[client_id]
+      account.transaction(**r)
+      write_accounts_file(account_file)
+      response = make_response(account.to_dict())
+    except KeyError as ek:
+      response = make_response({"status": "error", "message": "Client not found"})
+      response.status = 404
+    except TransactionError as e:
+      response = make_response({"status": "error", "message": f"{e}"})
+      response.status = 400
+    except WithdrawalBlocked as e:
+      response = make_response({"status": "error", "message": f"{e}"})
+      response.status = 403
+  except KeyError as ek:
+    response = make_response(
+      {
+        "status": "error",
+        "message": f"account type - {account_type}. Is a wrong account type. Must be one of {','.join(all_accounts.keys())}"
+      }
+    )
+    response.status = 404
+
+  return response
+
+@app.route("/api/v1/<string:account_type>", methods=["PUT"])
+def create_account(account_type):
+  accounts = {
+    **all_accounts["creditaccounts"],
+    **all_accounts["debitaccounts"]
+  }
   account = request.json
-  response = make_response({"status": "error", "message": "Client already has an account"})
-  response.status = 406 
-  if account["client_id"] not in accounts.keys():
-    client_id = account["client_id"]
-    if account["type"] == "credit":
-      accounts[client_id] = CreditAccount(**account)
-    elif account["type"] == "debit":
-      accounts[client_id] = DebitAccount(**account)
 
-    write_accounts_file(account_file, accounts)
+  if account["client_id"] in accounts.keys():
+    response = make_response({"status": "error", "message": "Client already has an account"})
+    response.status = 406
+    return response
 
-    response = make_response({"status": "ok", "message": f"Account for {client_id} created"})
-    response.status = 201
-    
+  client_id = account["client_id"]
+  if account_type == "creditaccounts":
+    account["type"] = "credit"
+    all_accounts[account_type][client_id] = CreditAccount(**account)
+  elif account_type == "debitaccounts":
+    account["type"] = "debit"
+    all_accounts[account_type][client_id] = DebitAccount(**account)
+  else:
+    response = make_response(
+      {
+        "status": "error",
+        "message": f"account type - {account_type}. Is a wrong account type. Must be one of {','.join(all_accounts.keys())}"
+      }
+    )
+    response.status = 404
+    return response
+
+  write_accounts_file(account_file)
+
+  response = make_response({"status": "ok", "message": f"Account for {client_id} created"})
+  response.status = 201
+
   return response
 
 
+all_accounts = {}
 account_file = "account.yaml"
-accounts = init()
+debit_accounts, credit_accounts = init()
+all_accounts["debitaccounts"] = debit_accounts
+all_accounts["creditaccounts"] = credit_accounts
